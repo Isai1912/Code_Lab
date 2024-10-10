e440dcf4225cc639649c429f7f755eb311c64bea
Isai1912 <117005659@pcpuma.acatlan.unam.mx>
2024-10-10 11:49:33 -0600

    Create HelloWorld.java
    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Main {
+    static void greet(){ 
+          JOptionPane.showMessageDialog(null, "Hello World!", "Welcome", JOptionPane.INFORMATION_MESSAGE);
+    }
+  
+    public static void main(String[] args){
+          greet();
+    }
+}
